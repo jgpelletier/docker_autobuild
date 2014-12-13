583984d6723a47888aee44cc5e6556c88bb1e910
Alan Gutierrez <alan@prettyrobots.com>
2014-12-13 11:21:14 -0800

    Delete line.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 /*
     ___ usage: en_US ___
     usage: node server.js
-    Launch an amalgamated agent configured and monitored by a wrapper process.
+
     ___ usage ___
 */
 
-console.log(process.argv.slice(2))
 
 var http = require('http');
 http.createServer(function (req, res) {
